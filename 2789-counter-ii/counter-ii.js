3f5ef0c2d9207306162a5cd45aa3a5eb60d61ca4
yel-aoun <87442827+yel-aoun@users.noreply.github.com>
2023-09-06 14:44:02 +0100

    Time: 63 ms (46.10%) | Memory: 44.6 MB (55.83%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let par = init;
+    return {
+        increment:() =>{
+            return ++par;
+        },
+        decrement:() =>{
+            return --par;
+        },
+        reset:() =>{
+            return (par = init);
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
