98ad5d4f802278c95b6c2dded0bbe30bcf3163bc
Youssef El aouny <87442827+yel-aoun@users.noreply.github.com>
2023-11-20 19:29:47 +0100

    Time: 56 ms (27.47%) | Memory: 42.4 MB (11.23%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+ let isCalled = false;
+ let result;
+    
+	return function(...args){
+        if (!isCalled){
+            result = fn(...args);
+            isCalled = true;
+            return result;
+        }else{
+            return undefined
+        }
+
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
